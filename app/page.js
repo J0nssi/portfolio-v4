9771a495bd8ab768f30f6bc9f43e4e8ef43b89af
J0nssi <joonas.vaija@hotmail.com>
2024-09-26 08:45:53 +0300

    Fixed a typo

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.info}>
         <p className={styles.header}>Info</p>
-        <p>Hello, I&apos;m Joonas, a Finnish software engineer living in Helsinki. I am crafting solid and smooth experiences with with TypeScript, C# and Python.</p>
+        <p>Hello, I&apos;m Joonas, a Finnish software engineer living in Helsinki. I'm crafting solid and smooth experiences with TypeScript, C# and Python.</p>
         <div className={styles.button}>
           <MdOutlineArrowOutward className={styles.buttonArrow} />
           <p className={styles.buttonText}><TransitionLink href="/about">Read more</TransitionLink></p>
